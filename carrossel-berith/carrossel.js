0472d2537c137bbf873c3af779ff9beb3653063a
maria0fernanda0 <mafe.dias06@gmail.com>
2025-06-30 17:44:22 -0300

    cards dos produtos feita, junto com a pagina politicas e termos.

--- carrossel-berith/carrossel.js
+++ carrossel-berith/carrossel.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 document.addEventListener('DOMContentLoaded', function () {
   const container = document.querySelector('.carrossel-track');
   const slides = document.querySelectorAll('.carrossel-slide');
@@ -48,55 +48,4 @@ document.addEventListener('DOMContentLoaded', function () {
 
   window.addEventListener('resize', mostrarSlide);
   mostrarSlide(false);
-=======
-document.addEventListener('DOMContentLoaded', function () {
-  const container = document.querySelector('.carrossel-track');
-  const slides = document.querySelectorAll('.carrossel-slide');
-  const proximoBotao = document.querySelector('.next');
-  const botaoAnterior = document.querySelector('.prev');
-
-  const ultimoIndiceSlide = slides.length - 2;
-  let slideAtual = 1;
-
-  function mostrarSlide(animado = true) {
-    if (slides.length === 0) return;
-
-    const larguraSlide = slides[0].offsetWidth;
-    if (!animado) {
-      container.style.transition = 'none';
-    } else {
-      container.style.transition = 'transform 0.5s ease-in-out';
-    }
-    container.style.transform = `translateX(-${slideAtual * larguraSlide}px)`;
-
-    if (animado) {
-      container.addEventListener('transitionend', lidarComTransicaoFinal, {
-        once: true,
-      });
-    }
-  }
-
-  function lidarComTransicaoFinal() {
-    if (slideAtual === ultimoIndiceSlide + 1) {
-      slideAtual = 1;
-      mostrarSlide(false);
-    } else if (slideAtual === 0) {
-      slideAtual = ultimoIndiceSlide;
-      mostrarSlide(false);
-    }
-  }
-
-  proximoBotao.addEventListener('click', () => {
-    slideAtual++;
-    mostrarSlide();
-  });
-
-  botaoAnterior.addEventListener('click', () => {
-    slideAtual--;
-    mostrarSlide();
-  });
-
-  window.addEventListener('resize', mostrarSlide);
-  mostrarSlide(false);
->>>>>>> a3711fd732656d371feced4210caec58ac932968
 });
