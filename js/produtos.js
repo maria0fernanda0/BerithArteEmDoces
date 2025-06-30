ccc2241a66cdbdbf482f3889dd50c152002d74c4
Ricardo Silva <126185776+rtheend@users.noreply.github.com>
2025-06-30 19:37:17 -0300

    mudança do caminho e alteração nos cards

--- js/produtos.js
+++ js/produtos.js
@@ -2,84 +2,84 @@ const bolos = [
     {
         id: 1,
         imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        titulo: 'Bolo Chá de bebê',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
+        id: 2,
+        imagem: '/assets/doces/BoloArraia.jpg',
         titulo: 'Bolo Arráia',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 3,
+        imagem: '/assets/doces/BoloBobEsponja.jpg',
+        titulo: 'Bolo Bob Esponja',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 4,
+        imagem: '/assets/doces/BoloBolsa.jpg',
+        titulo: 'Bolo Bolsa',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 5,
+        imagem: '/assets/doces/BoloCarros.jpg',
+        titulo: 'Bolo do Carros',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 6,
+        imagem: '/assets/doces/BoloCasaFantasma.jpg',
+        titulo: 'Bolo Casa Fantasma',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 7,
+        imagem: '/assets/doces/BoloChurrasco.jpg',
+        titulo: 'Bolo Churrasco',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 8,
+        imagem: '/assets/doces/BoloDinossauro.jpg',
+        titulo: 'Bolo de Dinossauro',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 9,
+        imagem: '/assets/doces/BoloDisney.jpg',
+        titulo: 'Bolo Disney',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 10,
+        imagem: '/assets/doces/BoloFazendinha.jpg',
+        titulo: 'Bolo Fazendinha',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 11,
+        imagem: '/assets/doces/BoloFundoDoMar.jpg',
+        titulo: 'Bolo Fundo do Mar',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 12,
+        imagem: '/assets/doces/BoloHalloween.jpg',
+        titulo: 'Bolo de Halloween',
         descricao: 'sobre o bolo',
         preco: 200
     }
@@ -90,43 +90,36 @@ const bolos = [
 const kits = [
     {
         id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        imagem: '/assets/doces/doceCarros.jpg',
+        titulo: 'Doce do Carros',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 2,
+        imagem: '/assets/doces/DoceOncas.jpg',
+        titulo: 'Doce de Onças',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 3,
+        imagem: '/assets/doces/DoceBobEsponja.jpg',
+        titulo: 'Doce do Bob Esponja',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 4,
+        imagem: '/assets/doces/DoceHerois.jpg',
+        titulo: 'Doce de Herois',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
-        descricao: 'sobre o bolo',
-        preco: 200
-    },
-    {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 5,
+        imagem: '/assets/doces/DoceSmurfs2.jpg',
+        titulo: 'Doce dos Smurfs',
         descricao: 'sobre o bolo',
         preco: 200
     },
@@ -135,43 +128,43 @@ const kits = [
 const doces = [
     {
         id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        imagem: '/assets/doces/KitDinossauro.jpg',
+        titulo: 'Kit de Dinossauto',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 2,
+        imagem: '/assets/doces/KitmesversarioCirco.jpg',
+        titulo: 'Kit Mesversario Circo',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 3,
+        imagem: '/assets/doces/kitMesversarioFazendinha.jpg',
+        titulo: 'Kit Mesversario Fazendinha',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 4,
+        imagem: '/assets/doces/KitMesversaioPuff.jpg',
+        titulo: 'Kit Mesversario Puff',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 5,
+        imagem: '/assets/doces/KitmesversarioRosa.jpg',
+        titulo: 'Kit Mesversario Rosa',
         descricao: 'sobre o bolo',
         preco: 200
     },
     {
-        id: 1,
-        imagem: '/assets/doces/BoloChadeBebe.jpg',
-        titulo: 'Bolo Chá de Bebê',
+        id: 6,
+        imagem: '/assets/doces/KitPanda.jpg',
+        titulo: 'Kit Panda',
         descricao: 'sobre o bolo',
         preco: 200
     },
